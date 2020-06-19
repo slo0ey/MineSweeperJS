dc244100e89ae6dad2047cf3aad5815200e7baf5
DenFade <rurchi1206@gmail.com>
2020-06-20 01:36:06 +0900

    Rebuild all code (without bugfix)

--- MineSweeper.js
+++ MineSweeper.js
@@ -6,108 +6,117 @@ exports.MineSweeper = function(){
         this.y = y|0;
         this.size = this.x * this.y;
         this.b = b|0;
-        this.table = new Array(this.size);
-        this.log = [];
-        this.startAt = 0;
-        this.endAt = 0;
-        for(var i = 0; i < this.size; i++){
-            this.table[i] = {
-                tile: 0,
-                flag: false,
-                opened: false
-            };
+        if(this.b > this.size) throw new TypeError("폭탄 갯수는 타일크기 이하여야 합니다.");
+        this.table = new Array(this.y);
+        for(var i = 0; i < this.y; i++){
+            this.table[i] = new Array(this.x);
+            for(var k = 0; k < this.x; k++){
+                this.table[i][k] = {
+                    tile: 0,
+                    opened: false,
+                    flag: false
+                }
+            }
         }
+        this.died = false;
     }
 
-    MineSweeper.prototype.valid = function(c){
-        let [cx, cy] = [m%this.y, (m/this.y)|0];
-        return (cx >= 0 && cx < this.x && cy >= 0 && cy < this.y);
+    MineSweeper.around = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
+
+    MineSweeper.prototype.safe = function(x, y){
+        return x >= 0 && x < this.x && y >= 0 && y < this.y;
     }
 
-    MineSweeper.BOMB = 'b';
-    MineSweeper.ALREADY_OPENED = -1;
-    MineSweeper.DIE = -2;
-    
+    MineSweeper.prototype.division = function(xy){
+        return [xy%this.y, xy/this.y];
+    }
+
+    MineSweeper.prototype.sum = function(x,  y){
+        return x+(y*this.x);
+    }
 
     MineSweeper.prototype.setMine = function(){
-        let bs = "0".repeat(this.size).split("").map((v, i) => i);
-        let mx = this.x;
-        
-        (function shuffle(){
-            var tmp;
-            for(var k = bs.length-1; k > 0; k--){
-                var k2 = (Math.random() * (k+1))|0;
-                tmp = bs[k]; bs[k] = bs[k2]; bs[k2] = tmp;
-            }//kakaotalk
-        }).bind(this)();
-        
-        bs = bs.slice(0, this.b);
-        Api.showToast(bs);
-        
-        for(var m of bs){
-            this.table[m].tile = MineSweeper.BOMB; 
-            let [nx, ny] = [m%this.y, (m/this.y)|0];
-            
-            this.valid(m-mx-1) && (this.table[m-mx-1]+=1);
-            this.valid(m-mx) && (this.table[m-mx]+=1);
-            this.valid(m-mx+1) && (this.table[m-mx+1]+=1);
-            this.valid(m-1) && (this.table[m-1]+=1);
-            this.valid(m+1) && (this.table[m+1]+=1);
-            this.valid(m+mx-1) && (this.table[m+mx-1]+=1);
-            this.valid(m+mx) && (this.table[m+mx]+=1);
-            this.valid(m+mx+1) && (this.table[m+mx+1]+=1);
+        var nums = new Array(this.size);
+        for(var i in nums) nums.push(i);
+        nums = nums.slice(0, this.b).forEach(v => {
+            return this.division(v);
+        });
+
+        for(var c in nums){
+            var cod = nums[c];
+            this.table[cod[1]][cod[0]].tile = 'b';
+
+            for(var a in MineSweeper.around){
+                var aro = MineSweeper.around[a];
+                var newcod = [cod[0]+aro[0], cod[1]+aro[1]];
+
+                if(this.table[newcod[1]][newcod[0]].tile !== 'b'){
+                    this.table[newcod[1]][newcod[0]].tile++;
+                }
+            }
         }
     }
 
-    MineSweeper.prototype.start = function() {
-        this.startAt = Date.now();
-    }
-
-    MineSweeper.prototype.end = function(){
-        this.endAt = Date.now();
+    MineSweeper.prototype.flag = function(x, y){
+        if(x < 0 || y < 0|| x >= this.x || y >= this.y) throw new TypeError("Invalid Coordinate");
+        if(!this.table[y][x].opened){
+            var f = this.table[y][x].flag;
+            this.table[y][x].flag = !f;
+        }
     }
 
     MineSweeper.prototype.open = function(x, y){
-        if(!this.startAt || this.endAt) throw new Error("지금은 타일을 열 수 없습니다.");
-        if(x < 1 || y < 1 || x > this.x || y > this.y) throw new Error("유효하지 않는 범위입니다.");
+        if(x < 0 || y < 0|| x >= this.x || y >= this.y) throw new TypeError("Invalid Coordinate");
+        var current = this.table[y][x].tile;
 
-        var c = ((y-1) * this.x) + (x-1);
-        var ct = this.table[c];
-        
-        if(ct.opened){
-            if(ct.tile == 0) return MineSweeper.ALREADY_OPENED;
+        if(this.table[y][x].opened){
+            if(current == 0) return;
             else {
-                var count = 0;
+                var count = current;
+                var subcount = 0;
+                for(var a in MineSweeper.around){
+                    var aro = MineSweeper.around[a];
+                    var newcod = [x+aro[0], y+aro[1]];
+    
+                    if(!this.table[newcod[1]][newcod[0]].opened && this.table[newcod[1]][newcod[0]].flag) subcount++;
+                }
+                if(subcount == count){
+                    for(var a in MineSweeper.around){
+                        var aro = MineSweeper.around[a];
+                        var newcod = [x+aro[0], y+aro[1]];
+        
+                        if(!this.table[newcod[1]][newcod[0]].opened){
+                            this.open(newcod[0], newcod[1]);
+                        }
+                    }
+                }
             }
-        }
-        else if(ct.tile == MineSweeper.BOMB){
-            this.end();
-            return MineSweeper.DIE;
-        }
-        else{
-            this.table[c].opened = true;
-            if(ct.tile === 0){
-                this.open(x-1, y-1);
-                this.open(x, y-1);
-                this.open(x+1, y-1);
-                this.open(x-1, y);
-                this.open(x+1, y);
-                this.open(x-1, y+1);
-                this.open(x, y+1);
-                this.open(x+1, y+1);
+        } else {
+            this.table[y][x].opened = true;
+            if(current == 0){
+                for(var a in MineSweeper.around){
+                    var aro = MineSweeper.around[a];
+                    var newcod = [x+aro[0], y+aro[1]];
+    
+                    if(!this.table[newcod[1]][newcod[0]].opened){
+                        this.open(newcod[0], newcod[1]);
+                    }
+                }
+            } else if(current == 'b'){
+                this.died = true;
+                return -1;
             }
         }
     }
 
     MineSweeper.prototype.toString = function(){
-        return "-- MineSweeper Info --\n"+
-        "\n"+
-        "X: "+this.x+"\n"+
-        "Y: "+this.y+"\n"+
-        "Bomb: "+this.b+"\n"+
-        "Table: \n\n"+this.table.reduce((ac, cv, ci) => ac += ci !== 0 && ci%this.x == 0 ? "\n"+cv.tile : cv.tile, "")+"\n\n"+
-        "StartAt: " + this.startAt;
+        var display = "";
+        for(var i = 0; i < this.y; i++){
+            display+="\n";
+            for(var k = 0; k < this.x; k++){
+                display+=this.table[i][k].tile
+            }
+        }
+        return "-- MineSweeper("+this.x+"x"+this.y+"/"+this.b+") --\n" + display;          
     }
-
-    return MineSweeper;
 }
